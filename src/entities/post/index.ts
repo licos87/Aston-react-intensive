ccdf73993df43b95a6afa192719430be7e854ebd
Sergey Ivanov <licos.87@mail.ru>
2025-07-03 15:16:54 +0300

    init: Инициализирует приложение на React + TS
    # Conflicts:
    #	README.md

---
+++ src/entities/post/index.ts
@@ -0,0 +1,4 @@
+import { PostCard } from "./ui/PostCard.tsx";
+import type { IPost } from '@/entities/post/model/types/post'
+
+export { PostCard, type IPost };
---
+++ src/shared/constants/index.js
@@ -0,0 +1,5 @@
+import {COMPANY_URL} from "./company";
+import {STUDENT} from "./student";
+import {TEACHERS} from "./teachers";
+
+export {COMPANY_URL, STUDENT, TEACHERS}
