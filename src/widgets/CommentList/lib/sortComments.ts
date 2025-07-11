35fd85d95e23d535e4054b2339b84a5c8466a335
Sergey Ivanov <licos.87@mail.ru>
2025-07-11 22:11:24 +0300

    fix: Правит логику сортировки

--- src/widgets/CommentList/lib/sortComments.ts
+++ src/widgets/CommentList/lib/sortComments.ts
@@ -1,3 +1,6 @@
 import type { CommentType } from '@/entities/Comment/model/types/CommentType.ts';
 
-export const sortComments = (list: CommentType[]) => list.sort((a,b)=> Number(a.date) - Number(b.date))
+export const sortComments = (list: CommentType[]) => {
+  const date = (str: string) =>new Date(str).getTime()
+  return [...list].sort((a,b)=> date(b.date) - date(a.date))
+}
