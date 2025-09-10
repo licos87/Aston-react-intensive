a409c197e031ae4366e2ff4a46567a2d45411281
Sergey Ivanov <licos.87@mail.ru>
2025-09-10 21:20:04 +0300

    Update loading middleware and theme switcher

--- src/app/providers/storeProvider/middlewares/loadingMiddleware.ts
+++ src/app/providers/storeProvider/middlewares/loadingMiddleware.ts
@@ -1,27 +1,33 @@
-import type { Middleware, UnknownAction, Dispatch } from '@reduxjs/toolkit';
+import {
+  type Middleware,
+  type UnknownAction,
+  type Dispatch,
+  isAction,
+} from '@reduxjs/toolkit';
 import { setIsLoading } from '@/entities/loadingStatus';
 
-export const loadingMiddleware: Middleware<object, Dispatch<UnknownAction>> =
-  (store) => {
+export const loadingMiddleware: Middleware<object, Dispatch<UnknownAction>> = (
+  store
+) => {
   let pendingRequests = 0;
 
-  return (next) =>
-    (action: UnknownAction) => {
+  return (next) => (action) => {
+    if (isAction(action)) {
+      const isPending = action.type.includes('pending');
+      const isFulfilled = action.type.includes('fulfilled');
+      const isRejected = action.type.includes('rejected');
 
-        const isPending = action.type.includes('pending');
-        const isFulfilled = action.type.includes('fulfilled');
-        const isRejected = action.type.includes('rejected');
+      if (isPending) {
+        pendingRequests++;
+        store.dispatch(setIsLoading(true));
+      }
 
-        if (isPending) {
-          pendingRequests++;
-          store.dispatch(setIsLoading(true));
-        }
+      if (isFulfilled || isRejected) {
+        pendingRequests = Math.max(0, pendingRequests - 1);
 
-        if (isFulfilled || isRejected) {
-          pendingRequests = Math.max(0, pendingRequests - 1);
-
-          if (pendingRequests === 0) store.dispatch(setIsLoading(false));
-        }
-      return next(action);
+        if (pendingRequests === 0) store.dispatch(setIsLoading(false));
+      }
+    }
+    return next(action);
   };
 };
